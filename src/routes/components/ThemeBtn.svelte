abbd106aad89c3eb2ee55f4609ebbff88768ae7f
unloco <amxmodx.unloco@gmail.com>
2023-10-15 06:24:18 +0100

    fix issues

--- src/routes/components/ThemeBtn.svelte
+++ src/routes/components/ThemeBtn.svelte
@@ -13,26 +13,26 @@
 	class:!outline-secondary-focus={$themeStore === theme}
 	on:click={setTheme}>
 	<span class='bg-base-100 text-base-content w-full cursor-pointer font-sans' data-theme='{theme}'>
-		<div class='grid grid-cols-5 grid-rows-3'>
-			<div class='bg-base-200 col-start-1 row-span-2 row-start-1'></div>
-			<div class='bg-base-300 col-start-1 row-start-3'></div>
-			<div class='bg-base-100 col-span-4 col-start-2 row-span-3 row-start-1 flex flex-col gap-1 p-2'>
-				<div class='font-bold'>{theme}</div>
-				<div class='flex flex-wrap gap-1'>
-					<div class='bg-primary flex aspect-square w-5 items-center justify-center rounded lg:w-6'>
-						<div class='text-primary-content text-sm font-bold'>A</div>
-					</div>
-					<div class='bg-secondary flex aspect-square w-5 items-center justify-center rounded lg:w-6'>
-						<div class='text-secondary-content text-sm font-bold'>A</div>
-					</div>
-					<div class='bg-accent flex aspect-square w-5 items-center justify-center rounded lg:w-6'>
-						<div class='text-accent-content text-sm font-bold'>A</div>
-					</div>
-					<div class='bg-neutral flex aspect-square w-5 items-center justify-center rounded lg:w-6'>
-						<div class='text-neutral-content text-sm font-bold'>A</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		<span class='grid grid-cols-5 grid-rows-3'>
+			<span class='bg-base-200 col-start-1 row-span-2 row-start-1'></span>
+			<span class='bg-base-300 col-start-1 row-start-3'></span>
+			<span class='bg-base-100 col-span-4 col-start-2 row-span-3 row-start-1 flex flex-col gap-1 p-2'>
+				<span class='font-bold'>{theme}</span>
+				<span class='flex flex-wrap gap-1'>
+					<span class='bg-primary flex aspect-square w-5 items-center justify-center rounded lg:w-6'>
+						<span class='text-primary-content text-sm font-bold'>A</span>
+					</span>
+					<span class='bg-secondary flex aspect-square w-5 items-center justify-center rounded lg:w-6'>
+						<span class='text-secondary-content text-sm font-bold'>A</span>
+					</span>
+					<span class='bg-accent flex aspect-square w-5 items-center justify-center rounded lg:w-6'>
+						<span class='text-accent-content text-sm font-bold'>A</span>
+					</span>
+					<span class='bg-neutral flex aspect-square w-5 items-center justify-center rounded lg:w-6'>
+						<span class='text-neutral-content text-sm font-bold'>A</span>
+					</span>
+				</span>
+			</span>
+		</span>
 	</span>
 </button>
