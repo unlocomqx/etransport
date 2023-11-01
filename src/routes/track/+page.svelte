74d0debaf4c2602b304b35b31f4e13cb63157dee
unloco <amxmodx.unloco@gmail.com>
2023-11-01 15:24:15 +0100

    create track route

---
+++ src/routes/track/+page.svelte
@@ -0,0 +1,81 @@
+<script lang='ts'>
+	import type { PageData } from './$types';
+	import 'ol/ol.css';
+	import Map from '$lib/components/map/Map.svelte';
+	import CenterMarker from '$lib/components/map/CenterMarker.svelte';
+	import TransportMarker from '$lib/components/map/TransportMarker.svelte';
+	import Icon from '@iconify/svelte';
+	import { toast } from 'svelte-sonner';
+	import Loading from '$lib/components/Loading.svelte';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	export let data: PageData;
+
+	let { latitude, longitude } = data;
+
+	$: groups = data.groups;
+
+	let state = 'idle';
+
+	async function update(context = 'click') {
+		const perm = await navigator.permissions.query({ name: 'geolocation' });
+
+		if (perm.state === 'denied') {
+			if (context === 'click') {
+				toast.error('Geolocation for this site is disabled. Please enable it in your browser settings.');
+			}
+			return;
+		}
+
+		if (context === 'click') {
+			state = 'loading';
+		}
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				latitude = position.coords.latitude;
+				longitude = position.coords.longitude;
+				state = 'idle';
+				goto(`/map?latitude=${latitude}&longitude=${longitude}`, { invalidateAll: true });
+			},
+			(err) => {
+				if (context === 'click') {
+					toast.error('Failed to get your location.');
+				}
+				console.error(err);
+				state = 'idle';
+			},
+			{
+				maximumAge: 0
+			}
+		);
+	}
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			update('refresh');
+		}, 3000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+</script>
+
+<Map center={{latitude, longitude}}>
+	<CenterMarker coords={{latitude, longitude}} />
+	{#each groups as group (group.id)}
+		<TransportMarker {group} />
+	{/each}
+	<button class='btn btn-circle btn-secondary fixed bottom-4 right-4 z-10 overflow-hidden' data-cy='update-position'
+					on:click={() => update()}>
+		<Icon class='text-2xl' icon='mdi:my-location' />
+		{#if state === "loading"}
+			<Loading />
+		{/if}
+	</button>
+	<button class='btn btn-circle btn-secondary fixed bottom-4 left-4 z-10 overflow-hidden'
+					on:click={() => goto('/')}>
+		<Icon class='text-2xl' icon='mdi:arrow-left' />
+	</button>
+</Map>
