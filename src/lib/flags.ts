1f1cd8ba1eccb41ca9dc4548211104bbbe7d2c0f
unloco <amxmodx.unloco@gmail.com>
2023-11-04 20:15:35 +0100

    Extract constants

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -1,5 +1,5 @@
 export const DEBUG = true;
 export const GROUP_RADIUS = 20;
 export const GROUP_TIMESPAN = 30;
-export const TRACKING_THRESHOLD = 1000 * 1;
+export const TRACKING_THRESHOLD = 1000;
 export const MARKER_LIFESPAN = 1000 * 60 * 60;
